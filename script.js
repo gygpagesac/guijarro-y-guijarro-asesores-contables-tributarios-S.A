66737ebee195722952f5b9b578866ecc027aa5bd
gygpagesac <guijarroyguijarrotk@gmail.com>
2026-03-03 14:23:35 -0500

    Update script.js

--- script.js
+++ script.js
@@ -3,4 +3,10 @@ const menu = document.getElementById("menu-new");
 toggle.addEventListener("click", () => {
   menu.classList.toggle("active");
 });
+// Duplicar imágenes del carrusel automáticamente
+const track = document.getElementById('carousel-track');
+if (track) {
+  const images = track.innerHTML;
+  track.innerHTML = images + images; // duplica solo una vez
+}
 
